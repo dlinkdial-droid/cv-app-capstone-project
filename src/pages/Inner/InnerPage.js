37415603c3798e1deeba54a3132f0deba7afe008
Nail Gabdulkhairov <dlinkdial@gmail.com>
2026-07-01 00:43:54 +0500

    done

--- src/pages/Inner/InnerPage.js
+++ src/pages/Inner/InnerPage.js
@@ -6,13 +6,12 @@ import TimeLine from "../../components/TimeLine/TimeLine";
 import Expertise from '../../components/Expertise/Expertise';
 import Portfolio from "../../components/Portfolio/Portfolio";
 import Address from "../../components/Address/Address";
-import Feedback from '../../components/Feedback/Feedback'
-import reporterAvatar from '../../assets/images/reporter_img.png'
+import Feedback from '../../components/Feedback/Feedback';
+import reporterAvatar from '../../assets/images/reporter_img.png';
 
 import "./InnerPage.scss";
 
 const Inner = () => {
-
     const expertiseMockData = [
         {
             date: "2013-2014",
@@ -32,10 +31,9 @@ const Inner = () => {
         }
     ];
 
-
     const feedbackData = [
         {
-            feedback: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor",
+            feedback: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. ",
             reporter: {
                 photoUrl: reporterAvatar,
                 name: "John Doe",
@@ -43,14 +41,15 @@ const Inner = () => {
             }
         },
         {
-            feedback: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor",
+            feedback: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. ",
             reporter: {
                 photoUrl: reporterAvatar,
                 name: "John Doe",
                 citeUrl: "https://www.citeexample.com"
             }
         }
-    ]
+    ];
+
     return (
         <div className="inner-page">
             <Panel />
@@ -65,8 +64,9 @@ const Inner = () => {
                 </section>
 
                 <section id="experience" className="inner-page__section">
-                    <Expertise title="Expertise" data={expertiseMockData} />
+                    <Expertise title="Experience" data={expertiseMockData} />
                 </section>
+
                 <section id="portfolio" className="inner-page__section">
                     <Portfolio />
                 </section>
@@ -76,9 +76,10 @@ const Inner = () => {
                 </section>
 
                 <section id="feedbacks" className="inner-page__section">
-                    <Feedback title="Feedback" data={feedbackData} />
+                    <Feedback title="Feedbacks" data={feedbackData} />
                 </section>
             </main>
+
             <BackToTop />
         </div>
     );

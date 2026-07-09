f56c001d1c7ea26d8aadade76872e3fe4a560f17
Nail Gabdulkhairov <dlinkdial@gmail.com>
2026-07-09 17:12:06 +0500

    done

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import makeServer from "./services/server";
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-if (process.env.NODE_ENV === "development") {
-  makeServer();
-}
+// if (process.env.NODE_ENV === "development") {
+
+// }
+
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +22,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
